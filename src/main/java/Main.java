d14954488e095f4ee16966c0fe5198cbce766b24
Alex Turovets <madlilu177@gmail.com>
2021-01-09 12:31:33 +0300

    First

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    BonusService service = new BonusService();
+
+    long amount = 1000_60;
+    boolean registered = true;
+
+    long actual = service.calculate(amount, registered);
+    System.out.println(actual);
+
+  }
+}
+
