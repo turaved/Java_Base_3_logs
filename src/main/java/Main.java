a26d02585604639d80b243c43835c0df19788733
Alex Turovets <madlilu177@gmail.com>
2021-01-09 14:07:33 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,9 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
-    //long actual = service.calculate(amount, registered);
-    //System.out.println(actual);
+    //service.calculate(amount, registered);
+    long actual = service.calculate(amount, registered);
+    System.out.println(actual);
 
   }
 }
